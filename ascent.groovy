9dbef0bc6d68fba33411ac6b542207f6784a3c90
blobb <blobb@blobbook>
2017-06-25 12:34:05 +0200

    Fix formatting + shellcheck report name is 'shellcheck.xml'

--- ascent.groovy
+++ ascent.groovy
@@ -9,29 +9,29 @@ freeStyleJob('boddenberg.it/ascent') {
 
 	triggers {
 		githubPush()
-  }
+	}
 
 	steps {
-		shell("shellcheck --format checkstyle *.sh | tee > ascent.xml")
+		shell("shellcheck --format checkstyle *.sh | tee > shellcheck.xml")
 	}
 
 	publishers {
-		checkstyle('ascent.xml') {
-    healthLimits(3, 20)
-    thresholdLimit('high')
-    defaultEncoding('UTF-8')
-    canRunOnFailed(true)
-    useStableBuildAsReference(true)
-    useDeltaValues(true)
-    computeNew(true)
-    shouldDetectModules(true)
-    thresholds(
-			unstableTotal: [all: 9, high: 10, normal: 11, low: 12],
-			failedTotal: [all: 13, high: 14, normal: 15, low: 16],
-			unstableNew: [all: 1, high: 2, normal: 3, low: 4],
-      failedNew: [all: 5, high: 6, normal: 7, low: 8]
-    	)
-    }
-		archiveArtifacts('ascent.*')
-  }
+		checkstyle('shellcheck.xml') {
+			healthLimits(3, 20)
+			thresholdLimit('high')
+			defaultEncoding('UTF-8')
+			canRunOnFailed(true)
+			useStableBuildAsReference(true)
+			useDeltaValues(true)
+			computeNew(true)
+			shouldDetectModules(true)
+			thresholds(
+				unstableTotal: [all: 9, high: 10, normal: 11, low: 12],
+				failedTotal: [all: 13, high: 14, normal: 15, low: 16],
+				unstableNew: [all: 1, high: 2, normal: 3, low: 4],
+				failedNew: [all: 5, high: 6, normal: 7, low: 8]
+			)
+		}
+		archiveArtifacts('ascent.*, shellcheck.xml')
+	}
 }
--- asrec.groovy
+++ asrec.groovy
@@ -9,30 +9,30 @@ freeStyleJob('boddenberg.it/asrec') {
 
 	triggers {
 		githubPush()
-  }
+	}
 
 	steps {
-		shell("shellcheck --format checkstyle *.sh | tee > asrec.xml")
+		shell("shellcheck --format checkstyle *.sh | tee > shellcheck.xml")
 		//shell("./build.sh test") FIRST ACTIVATE X11 for build user
 	}
 
 	publishers {
-  	checkstyle('asrec.xml') {
-    healthLimits(3, 20)
-  	thresholdLimit('high')
-  	defaultEncoding('UTF-8')
-  	canRunOnFailed(true)
-  	useStableBuildAsReference(true)
-  	useDeltaValues(true)
-  	computeNew(true)
-  	shouldDetectModules(true)
-    thresholds(
-			unstableTotal: [all: 9, high: 10, normal: 11, low: 12],
-			failedTotal: [all: 13, high: 14, normal: 15, low: 16],
-			unstableNew: [all: 1, high: 2, normal: 3, low: 4],
-    	failedNew: [all: 5, high: 6, normal: 7, low: 8]
-    	)
-    }
-		archiveArtifacts('asrec.groovy, build.sh, manifest')
-  }
+		checkstyle('shellcheck.xml') {
+			healthLimits(3, 20)
+			thresholdLimit('high')
+			defaultEncoding('UTF-8')
+			canRunOnFailed(true)
+			useStableBuildAsReference(true)
+			useDeltaValues(true)
+			computeNew(true)
+			shouldDetectModules(true)
+			thresholds(
+				unstableTotal: [all: 9, high: 10, normal: 11, low: 12],
+				failedTotal: [all: 13, high: 14, normal: 15, low: 16],
+				unstableNew: [all: 1, high: 2, normal: 3, low: 4],
+				failedNew: [all: 5, high: 6, normal: 7, low: 8]
+			)
+		}
+		archiveArtifacts('asrec.*, shellcheck.xml, build.sh, manifest')
+	}
 }
